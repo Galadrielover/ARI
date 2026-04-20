de8eef6ef754cdb2f7c1f4b6d25dae3f7d8c0269
Galadrielover <ajhaug52829@gmail.com>
2026-04-20 08:37:58 -0400

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import base44 from '@base44/vite-plugin'
+import path from 'path'
+
+export default defineConfig({
+  logLevel: 'error',
+  plugins: [
+    base44({
+      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
+      hmrNotifier: false,
+      navigationNotifier: false,
+      analyticsTracker: false,
+      visualEditAgent: false
+    }),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  }
+})
